67d747226389c71657427725b06d83f9f1416671
Cedric Giermanns <cgiermanns@gmail.com>
2018-12-05 13:38:02 +0100

    Upload Program

---
+++ EntityFramework/Models.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Collections.Generic;
+
+namespace ConsoleApp.NewDb
+{
+    public class BloggingContext : DbContext
+    {
+        private object options;
+
+        public BloggingContext()
+        {
+        }
+
+        public BloggingContext(object options)
+        {
+            this.options = options;
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+                optionsBuilder.ConfigureWarnings(warnigs => warnigs.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            }
+        }
+    }
+}
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "https://blogs.msdn.microsoft.com/adonet/page/2/" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+                Console.WriteLine("Press a Key to Exit!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
