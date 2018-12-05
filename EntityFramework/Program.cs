f3b45c483bbb8c7d1215ac8f6cf846d2e70a56c2
Cedric Giermanns <cgiermanns@gmail.com>
2018-12-05 14:25:11 +0100

    Add SwitchCAse

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -7,6 +7,27 @@ namespace ConsoleApp.NewDb
     {
         static void Main(string[] args)
         {
+            Boolean run = false;
+            int caseSwitch = 0;
+            int id = 0;
+
+            while (run)
+            {
+                Console.WriteLine("Entity Framework");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Insert");
+                Console.WriteLine("Delet");
+                if (int.TryParse(Console.ReadLine(), out caseSwitch) && caseSwitch <= 3 && caseSwitch != 0)
+                {
+                    Console.Clear();
+                }
+                switch (caseSwitch)
+                {
+                    case 1:
+                        break;
+                }
+            }
+
             using (var db = new BloggingContext())
             {
                 db.Blogs.Add(new Blog { Url = "https://blogs.msdn.microsoft.com/adonet/page/2/" });
@@ -20,6 +41,12 @@ namespace ConsoleApp.NewDb
                 }
                 Console.WriteLine("Press a Key to Exit!");
                 Console.ReadKey();
+            }
+            var url = new Blog() { BlogId = id };
+            using (var context = new BloggingContext())
+            {
+                context.Entry(url).State = System.Data.Entety;
+                context.SaveChanges();
             }
         }
     }
